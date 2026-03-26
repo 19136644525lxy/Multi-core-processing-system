b0fd3a5e31eb61b193159d71c90ee327acbff4ef
Administrator <2350879980@qq.com>
2026-03-26 10:19:41 +0800

    Add K key binding for MCPS main menu and fix language file duplicates

--- src/main/java/com/qituo/mcps/client/MCPSClient.java
+++ src/main/java/com/qituo/mcps/client/MCPSClient.java
@@ -1,13 +1,35 @@
 package com.qituo.mcps.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import com.qituo.mcps.render.FogManager;
 
 public class MCPSClient implements ClientModInitializer {
+    private KeyBinding openMainMenuKey;
     
     @Override
     public void onInitializeClient() {
         // 初始化雾管理器
         FogManager.initialize();
+        
+        // 注册按键绑定
+        openMainMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.mcps.menu",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_K,
+            "category.mcps"
+        ));
+        
+        // 注册客户端 tick 事件
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openMainMenuKey.wasPressed()) {
+                // 打开主菜单界面
+                client.setScreen(new MCPSMainMenu());
+            }
+        });
     }
 }
